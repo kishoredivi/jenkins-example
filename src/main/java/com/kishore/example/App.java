d17e04db50b4d08ac87742ebbdd85601fe7f834b
kishoredivi <kissu.bits@gmail.com>
2018-06-01 00:19:25 +0530

    Update App.java

--- src/main/java/com/kishore/example/App.java
+++ src/main/java/com/kishore/example/App.java
@@ -7,8 +7,7 @@ package com.kishore.example;
 public class App 
 {
     public static void main( String[] args )
-    {
-                
+    {          
         System.out.println( "Hello World!" );
 
     }
